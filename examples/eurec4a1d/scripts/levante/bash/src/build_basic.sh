84c73506df08eefb453b5d28e2894f547417699a
clara.bayley <clara.bayley@mpimet.mpg.de>
2026-07-27 16:55:07 +0200

    feat!: replace scripts bash folder with one in examples
    new bash scripts copied from CLEO v0.65.1

--- examples/eurec4a1d/scripts/levante/bash/src/build_basic.sh
+++ examples/eurec4a1d/scripts/levante/bash/src/build_basic.sh
@@ -17,39 +17,42 @@ source ${bashsrc}/levante_packages.sh
 
 if [ "${CLEO_COMPILERNAME}" == "intel" ]
 then
-  module load ${levante_intel}
-  spack load ${levante_intel_openmpi}
+  module load ${levante_intel} ${levante_intel_openmpi}
   spack load ${levante_intel_cmake}
-  export CLEO_CXX_COMPILER=${levante_icpc_compiler}
-  export CLEO_CC_COMPILER=${levante_icc_compiler}
+  export CLEO_CXX_COMPILER="$(command -v mpic++)"
+  export CLEO_CC_COMPILER="$(command -v mpicc)"
 
   if [ "${CLEO_ENABLEDEBUG}" == "true" ]
   then
     ### for correctness and debugging (note -gdwarf-4 not possible for nvc++) use:
-    export CLEO_CXX_FLAGS="${CLEO_CXX_FLAGS} -Werror -Wall -Wextra -pedantic -Wno-unused-parameter -g -gdwarf-4 -O0" # correctness and debugging
+    export CLEO_CXX_FLAGS="${CLEO_CXX_FLAGS} -Werror -Wall -Wextra \
+      -pedantic -Wno-unused-parameter -g -gdwarf-4 -O0" # correctness and debugging
   else
     ### for performance use:
-    export CLEO_CXX_FLAGS="${CLEO_CXX_FLAGS} -Werror -Wall -Wextra -pedantic -Wno-unused-parameter -O3 -fma"
+    export CLEO_CXX_FLAGS="${CLEO_CXX_FLAGS} -Werror -Wall -Wextra \
+      -pedantic -Wno-unused-parameter -O3 -fma"
   fi
 elif [ "${CLEO_COMPILERNAME}" == "gcc" ]
 then
-  module load ${levante_gcc}
-  spack load ${levante_gcc_openmpi}
+  module load ${levante_gcc} ${levante_gcc_openmpi}
   spack load ${levante_gcc_cmake}
-  export CLEO_CXX_COMPILER=${levante_gxx_compiler}
-  export CLEO_CC_COMPILER=${levante_gcc_compiler}
+  export CLEO_CXX_COMPILER="$(command -v mpic++)"
+  export CLEO_CC_COMPILER="$(command -v mpicc)"
 
   if [ "${CLEO_ENABLEDEBUG}" == "true" ]
   then
     ### for correctness and debugging (note -gdwarf-4 not possible for nvc++) use:
-    export CLEO_CXX_FLAGS="${CLEO_CXX_FLAGS} -Werror -Wno-unused-parameter -Wall -Wextra -pedantic -g -gdwarf-4 -O0 -mpc64"
+    export CLEO_CXX_FLAGS="${CLEO_CXX_FLAGS} -Werror -Wno-unused-parameter -Wall -Wextra \
+      -pedantic -g -gdwarf-4 -O0 -mpc64"
   else
     ### for performance use:
-    export CLEO_CXX_FLAGS="${CLEO_CXX_FLAGS} -Werror -Wall -Wextra -pedantic -Wno-unused-parameter -O3 -mfma"
+    export CLEO_CXX_FLAGS="${CLEO_CXX_FLAGS} -Werror -Wall -Wextra \
+      -pedantic -Wno-unused-parameter -O3 -mfma"
   fi
 fi
 ### ---------------------------------------------------- ###
 
 ### ------------ choose basic kokkos flags ------------- ###
-export CLEO_KOKKOS_BASIC_FLAGS="${CLEO_KOKKOS_BASIC_FLAGS} -DKokkos_ARCH_NATIVE=ON -DKokkos_ENABLE_SERIAL=ON"
+export CLEO_KOKKOS_BASIC_FLAGS="${CLEO_KOKKOS_BASIC_FLAGS} \
+  -DKokkos_ARCH_NATIVE=ON -DKokkos_ENABLE_SERIAL=ON"
 ### ---------------------------------------------------- ###
