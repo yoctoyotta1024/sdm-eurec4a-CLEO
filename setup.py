364e6b9049ea573e0d0091376c5d3eed80fc2ed0
clara.bayley <clara.bayley@mpimet.mpg.de>
2026-07-27 16:27:20 +0200

    refactor!: delete all CLEO libraries from repo
    BREAKING CHANGE: CLEO must now be fetched externally

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-"""
------ CLEO -----
-File: setup.py
-Project: CLEO
-Created Date: Thursday 12th October 2023
-Author: Clara Bayley (CB)
-Additional Contributors:
------
-Last Modified: Wednesday 1st May 2024
-Modified By: CB
------
-License: BSD 3-Clause "New" or "Revised" License
-https://opensource.org/licenses/BSD-3-Clause
------
-Copyright (c) 2023 MPI-M, Clara Bayley
-"""
-
 from setuptools import setup, find_packages
 
 setup(
